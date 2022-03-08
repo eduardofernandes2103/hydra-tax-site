34cd3f5fb38c5f9072adfb7ed73cea45a99486db
eduardofernandes2103 <eduardofernandes21031@gmail.com>
2022-03-08 14:41:34 -0300

    DayTradeCalc refacted

--- src/pages/DayTradeCalc/DayTradeCalc.tsx
+++ src/pages/DayTradeCalc/DayTradeCalc.tsx
@@ -1,107 +1,49 @@
-import { useState, useEffect } from 'react';
-
-import * as yup from 'yup';
-import { yupResolver } from '@hookform/resolvers/yup';
-import { toast, ToastContainer } from 'react-toastify';
-import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import 'react-toastify/dist/ReactToastify.css';
 
 import Button from '../../components/Button/Button';
 import ComissionNote from '../../components/ComissionNote/ComissionNote';
-import { ComissionNoteProps } from '../../components/ComissionNote/ComissionNoteProps';
 import { Container } from './styles';
+import FormComponent from '../../components/FormComponent/FormComponent';
 import Header from '../../components/Header/Header';
-import Input from '../../components/Input/Input';
+import { currencyFormat } from '../../utils/format';
+import { useResult } from '../../providers/resultProvider';
+import { useComission } from '../../providers/comissionNotesProvider';
 
 const DayTradeCalc = () => {
   const { t } = useTranslation();
   const navigate = useNavigate();
+  const { insertResults } = useResult()
+  const { comissionNotes, removeComission } = useComission()
 
-  const [isNegative, setIsNegative] = useState<boolean>(false)
-  const [comissionNotes, setComissionNotes] = useState<ComissionNoteProps[]>([])
+  const handleGenerateDarf = () => {
+    const positiveNotes = comissionNotes.filter((note) => note.notePositiveValue)
+    const negativeNotes = comissionNotes.filter((note) => note.noteNegativeValue)
 
-  const formSchema = yup.object().shape({
-    comissionNoteDate: yup.string().required('Passe a data da nota em questão'),
-    notePositiveValue: yup.string(),
-    noteNegativeValue: yup.string(),
-    taxIncluded: yup.string()
-  });
+    const days = comissionNotes.length
+    const positiveTotal = positiveNotes.reduce((result, acc) => parseInt(currencyFormat(acc.notePositiveValue)) + result, 0)
+    const negativeTotal = negativeNotes.reduce((result, acc) => parseInt(currencyFormat(acc.noteNegativeValue)) + result, 0)
+    const totalIrTax = positiveNotes.reduce((result, acc) => parseInt(currencyFormat(acc.taxIncluded)) + result, 0)
+    const profit = positiveTotal - negativeTotal
+    const darf = (profit * 0.20) - totalIrTax
 
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState: { errors },
-  } = useForm<any>({
-    resolver: yupResolver(formSchema),
-  });
-
-  const onSubFunction = ({comissionNoteDate, notePositiveValue, noteNegativeValue, taxIncluded}: ComissionNoteProps) => {
-    const newId =
-    comissionNotes.length > 0 ? comissionNotes[comissionNotes.length - 1].id + 1 : 0;
-    const newNote: ComissionNoteProps = {
-      id: newId,
-      comissionNoteDate: comissionNoteDate,
-      notePositiveValue: notePositiveValue,
-      noteNegativeValue: noteNegativeValue,
-      taxIncluded: taxIncluded
+    const result = {
+      days: days,
+      positiveTotal: positiveTotal,
+      negativeTotal: negativeTotal,
+      profit: profit,
+      totalIrTax: totalIrTax,
+      darfValue: darf
     }
 
-    const newNoteList: ComissionNoteProps[] = [...comissionNotes, newNote]
+    insertResults(result)
 
-    setComissionNotes(newNoteList)
-
-    reset()
-  };
-
-  const handleRmoveNote = (id: number) => {
-    const newNoteList = comissionNotes.filter((note) => note.id !== id);
-    setComissionNotes(newNoteList)
-  }
-
-  const condicionalInputRender = () => {
-    return (
-      !isNegative ? (
-        <div>
-          <p>{t("dayTrade.positiveNoteLiquid")}</p>
-          <Input
-            mask='currency'
-            placeholder='Digite o lucro liquido da nota'
-            name='notePositiveValue'
-            register={register}
-            width="350px"
-            color={"var(--green)"}
-          />
-
-          <p>{t("dayTrade.includeTax")}</p>
-          <Input
-            mask='currency'
-            placeholder='Digite o valor do IR retido'
-            name='taxIncluded'
-            register={register}
-            width="350px"
-          />
-        </div>
-      ) : (
-        <div>
-          <p>{t("dayTrade.negativeNoteLiquid")}</p>
-          <Input
-            mask='currency'
-            placeholder='Digite o prejuízo liquido da nota'
-            name='noteNegativeValue'
-            register={register}
-            width="350px"
-            color={"var(--red)"}
-          />
-        </div>
-      )
-    )
+    navigate('/result')
   }
 
   const renderNotes = () => {
-    return comissionNotes?.map((note, index) => {
+    return comissionNotes.map((note, index) => {
       return (
         <ComissionNote
           id={note.id}
@@ -109,19 +51,14 @@ const DayTradeCalc = () => {
           comissionNoteDate={note.comissionNoteDate}
           noteValue={note.noteNegativeValue ? ` -${note.noteNegativeValue}` : note.notePositiveValue}
           taxIncluded={note.taxIncluded}
-          onClick={() => handleRmoveNote(note.id)}
+          onClick={() => removeComission(note.id)}
         />
       )
     })
   }
 
-  useEffect(() => {
-    toast.error(errors.comissionNoteDate?.message);
-  }, [errors]);
-
   return (
     <Container>
-      <ToastContainer />
       <Header />
       <div className="buttonPlace">
         <Button
@@ -130,66 +67,13 @@ const DayTradeCalc = () => {
           setFont="18px"
           setHeight="30px"
           setWidth="120px"
-          onClick={() => navigate('/result')}
+          onClick={() => handleGenerateDarf()}
         >
           {t('button.result')}
         </Button>
       </div>
       <div className="componentMain">
-        <div className='leftSide'>
-          <div>
-            <h2>{t('dayTradeCalc.leftSideTitle')}</h2>
-            <div className='conditionalButtons'>
-              <Button
-                setBackground="var(--lime)"
-                setColor="var(--white)"
-                setFont="18px"
-                setHeight="30px"
-                setWidth="120px"
-                onClick={() => setIsNegative(false)}
-              >
-                {t('button.isPositive')}
-              </Button>
-              <Button
-                setBackground="var(--red)"
-                setColor="var(--white)"
-                setFont="18px"
-                setHeight="30px"
-                setWidth="120px"
-                onClick={() => setIsNegative(true)}
-              >
-                {t('button.isNegative')}
-              </Button>
-            </div>
-          </div>
-          <form onSubmit={handleSubmit(onSubFunction)}>
-            <div>
-              <p>{t("dayTrade.dateLabel")}</p>
-              <Input
-                mask='comissionDate'
-                placeholder='dd/MM'
-                name='comissionNoteDate'
-                register={register}
-                width="350px"
-                error={errors.comissionNoteDate ? true : false}
-              />
-            </div>
-
-            {condicionalInputRender()}
-
-            <Button
-              setBackground="var(--sky-blue)"
-              setColor="var(--white)"
-              setFont="18px"
-              setHeight="30px"
-              setWidth="120px"
-              type='submit'
-            >
-              {t('button.registerNote')}
-            </Button>
-
-          </form>
-        </div>
+        <FormComponent title="dayTradeCalc.leftSideTitle" />
         <div className='rightSide'>
           <h2>{t('dayTradeCalc.rightSideTitle')}</h2>
           <div className='notesDisplay'>
